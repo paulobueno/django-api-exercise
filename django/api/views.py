a48b9f232d7a55396d6593e842aefeac25e1be42
Paulo Bueno <paulob.bruno@gmail.com>
2024-03-10 13:49:33 -0300

    modifying PropertyViewSet to list and get one register based on property_code

--- django/api/views.py
+++ django/api/views.py
@@ -5,10 +5,5 @@ from .serializers import PropertySerializer
 class PropertyViewSet(viewsets.ModelViewSet):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
-
-    def list(self, request, *args, **kwargs):
-        property_code = self.request.query_params.get('property_code', None)
-
-        if property_code:
-            self.queryset = Property.objects.filter(property_code=property_code)
-        return super().list(request, *args, **kwargs)
+    lookup_field = 'property_code'
+    lookup_value_regex = '[A-Z]{4}\d{4}'
